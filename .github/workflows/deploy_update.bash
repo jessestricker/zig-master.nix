959cae0864906eebcb2fb0b24c9d207e435eb27a
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2022-05-25 17:23:07 +0200

    ci: improve update commit message

--- .github/workflows/deploy_update.bash
+++ .github/workflows/deploy_update.bash
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-latest_version=$(jq -r '.version' index.json)
-tag_name="v${latest_version}"
+version=$(jq -r '.version' index.json)
+date=$(jq -r '.date' index.json)
+tag_name="v${version}"
 
-git commit -a -m "update: ${tag_name}"
+git commit -a -m "update: ${tag_name} (${date})"
 git tag "$tag_name"
 git push origin HEAD "refs/tags/${tag_name}"
