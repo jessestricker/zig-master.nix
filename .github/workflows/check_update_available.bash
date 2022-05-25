3c65c31f17684d99c6ca6ac6a42cd20547bcc44f
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2022-05-25 17:15:49 +0200

    ci: improve summary output

--- .github/workflows/check_update_available.bash
+++ .github/workflows/check_update_available.bash
@@ -14,7 +14,9 @@ updated_date=$(jq -r '.date' index.json)
 update_available=$([[ "$current_version" != "$updated_version" ]] && echo 'true' || echo 'false')
 echo "::set-output name=update_available::${update_available}"
 
-echo "# Summary" >> $GITHUB_STEP_SUMMARY
-echo "* current: ${current_version} (${current_date})" >> $GITHUB_STEP_SUMMARY
-echo "* updated: ${updated_version} (${updated_date})" >> $GITHUB_STEP_SUMMARY
-echo "update available: ${update_available}" >> $GITHUB_STEP_SUMMARY
+echo "| state   | version            | date            |" >> $GITHUB_STEP_SUMMARY
+echo "| ------- | ------------------ | --------------- |" >> $GITHUB_STEP_SUMMARY
+echo "| current | ${current_version} | ${current_date} |" >> $GITHUB_STEP_SUMMARY
+echo "| updated | ${updated_version} | ${updated_date} |" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+echo "update available: __${update_available}__" >> $GITHUB_STEP_SUMMARY
