7074c5f500046f78cf7513c0bd8faf5b6978c28b
Jesse Stricker <git@jessestricker.de>
2022-10-31 08:21:56 +0100

    style: run `shellcheck -o all` and `shfmt -s -w -sr`

--- .github/workflows/check_update_available.bash
+++ .github/workflows/check_update_available.bash
@@ -1,27 +1,33 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-INDEX_URL='https://ziglang.org/download/index.json'
+index_url='https://ziglang.org/download/index.json'
 
 # get current version and date
-current_version=$(jq -r '.version' index.json)
-current_date=$(jq -r '.date' index.json)
+current_version=$(jq -r ".version" index.json)
+current_date=$(jq -r ".date" index.json)
 
 # update index
-curl -s "$INDEX_URL" | jq '.master' > index.json
+curl -s "${index_url}" | jq ".master" > index.json
 
 # get updated version and date
-updated_version=$(jq -r '.version' index.json)
-updated_date=$(jq -r '.date' index.json)
+updated_version=$(jq -r ".version" index.json)
+updated_date=$(jq -r ".date" index.json)
 
 # check if update is available
-update_available=$([[ $current_version != "$updated_version" ]] && echo 'true' || echo 'false')
-echo "update_available=${update_available}" >> $GITHUB_OUTPUT
+if [[ ${current_version} != "${updated_version}" ]]; then
+	update_available="true"
+else
+	update_available="false"
+fi
+echo "update_available=${update_available}" >> "${GITHUB_OUTPUT}"
 
 # build GitHub step summary
-echo "| state   | version            | date            |" >> $GITHUB_STEP_SUMMARY
-echo "| ------- | ------------------ | --------------- |" >> $GITHUB_STEP_SUMMARY
-echo "| current | ${current_version} | ${current_date} |" >> $GITHUB_STEP_SUMMARY
-echo "| updated | ${updated_version} | ${updated_date} |" >> $GITHUB_STEP_SUMMARY
-echo "" >> $GITHUB_STEP_SUMMARY
-echo "update available: __${update_available}__" >> $GITHUB_STEP_SUMMARY
+{
+	echo "| state   | version            | date            |"
+	echo "| ------- | ------------------ | --------------- |"
+	echo "| current | ${current_version} | ${current_date} |"
+	echo "| updated | ${updated_version} | ${updated_date} |"
+	echo ""
+	echo "update available: __${update_available}__"
+} >> "${GITHUB_STEP_SUMMARY}"
--- .github/workflows/deploy_update.bash
+++ .github/workflows/deploy_update.bash
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-version=$(jq -r '.version' index.json)
-date=$(jq -r '.date' index.json)
+version=$(jq -r ".version" index.json)
+date=$(jq -r ".date" index.json)
 tag_name="v${version}"
 
 git commit -a -m "update: ${tag_name} (${date})"
-git tag "$tag_name"
+git tag "${tag_name}"
 git push origin HEAD "refs/tags/${tag_name}"
